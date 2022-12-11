8495e259e48c6a63c8f9a34aedde84a44601cb82
Tanya Matseonzhek <matseonzhek@gmail.com>
2022-12-11 18:51:17 +0400

    stage2-module1 DONE

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,19 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
+        int k = 0;
+        while (k <= args.size()) {
+            if (!StringUtils.isPositiveNumber(args.get(k))) {
+                return false;
+            }
+            k++;
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,8 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        //here magic will happen
+        int integer = Integer.parseInt(str);
+        return integer > 0;
     }
 }
